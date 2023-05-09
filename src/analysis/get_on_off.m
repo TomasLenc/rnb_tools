a729e4a52db9bd071b7f9a322d2fdea69d5985d2
Tomas Lenc <tomas.lenc@gmail.com>
2023-05-09 20:14:32 +0200

    add on/off contrast analysis

---
+++ src/analysis/get_on_off.m
@@ -0,0 +1,93 @@
+function [on_off_contrast] = get_on_off(x, fs, pulse_period_sec, win_dur, varargin)
+% Calculate phase stability of narrowband-filtered signal phase across pulse 
+% positions. 
+% 
+% Parameters
+% ----------
+% x : array_like, shape=[..., time]
+%     Time-domain signal to analyse. Time must be the last dimension. 
+% fs : float
+%     Sampling rate (samples/s)
+% pulse_period_sec : float
+%     Period of the pulse that is going to be analysed (in seconds). 
+% win_dur : float
+%     Duration of the analysis window starting at 'on'- and 'off'-beat
+%     positions. Mean value of the signal within these windows will be taken to
+%     calculate the on/off contrast. 
+% on_win_offset : float, optional, default=0
+%     Offset of the on-beat window in seconds, with respect to the beat
+%     time. 
+% off_win_offset : float, optional, default=pulse_period_sec/2
+%     Offset of the off-beat window in seconds, with respect to the beat
+%     time. 
+% pulse_phase_sec : float, optional, default=0
+%     Phase of the pulse that is going to be analysed (in seconds). This is with
+%     respect to the start of the signal (i.e. time of the first pulse position 
+%     after begining of the signal).   
+% 
+% Returns 
+% -------
+% on_off_contrast : float
+%     Michelson contrast of mean signal value in the on- vs. off-beat windows. 
+
+
+parser = inputParser; 
+
+addParameter(parser, 'pulse_phase_sec', 0); 
+addParameter(parser, 'on_win_offset', 0); 
+addParameter(parser, 'off_win_offset', pulse_period_sec/2); 
+
+parse(parser, varargin{:});
+
+pulse_phase_sec = parser.Results.pulse_phase_sec; 
+on_win_offset = parser.Results.on_win_offset; 
+off_win_offset = parser.Results.off_win_offset; 
+
+%% 
+
+win_n = round(win_dur * fs); 
+
+shape = size(x); 
+
+x_dur = size(x, ndims(x)) / fs; 
+pulse_pos_sec = [0 + pulse_phase_sec : pulse_period_sec : x_dur-1/fs]; 
+
+on_off_contrast = nan([shape(1:end-1), length(pulse_pos_sec)]); 
+
+% go over each individual pulse position
+for i_pulse=1:length(pulse_pos_sec)
+
+    % get mean feature value in the on-beat window
+    idx_start = round((pulse_pos_sec(i_pulse) + on_win_offset) * fs) + 1; 
+
+    index = repmat({':'}, 1, ndims(x)); 
+    index{end} = [idx_start : idx_start+win_n]; 
+    val_on = mean(x(index{:}), ndims(x)); 
+
+    % get mean feature value in the off-beat window
+    idx_start = round((pulse_pos_sec(i_pulse) + off_win_offset) * fs) + 1; 
+
+    index = repmat({':'}, 1, ndims(x)); 
+    index{end} = [idx_start : idx_start+win_n]; 
+    val_off = mean(x(index{:}), ndims(x)); 
+
+    % get Mitchelson contrast on-vs-off beat
+    index = repmat({':'}, 1, ndims(x)); 
+    index{end} = i_pulse; 
+    on_off_contrast(index{:}) = (val_on - val_off) ./ (val_on + val_off); 
+
+    
+end
+
+
+on_off_contrast = mean(on_off_contrast, ndims(x)); 
+
+
+
+
+
+
+
+
+
+
---
+++ tests/test_on_off.m
@@ -0,0 +1,152 @@
+function tests = test_on_off
+    tests = functiontests(localfunctions);
+end
+
+
+function test_get_on_off_zeros(test_case)
+    
+    trial_dur = 60;
+    fs = 500;
+
+    x = zeros(10, 64, 1, 1, 1, round(trial_dur * fs)); 
+    
+    [on_off_contrast] = get_on_off(x, fs, 0.8, 0.050);
+    
+    % check that the resulting vector length is small 
+    assert(all(isnan(on_off_contrast(:))));
+
+end
+
+
+function test_get_on_off_perfect_periodic(test_case)
+    
+    fs = 500;
+    
+    ir = get_square_kernel(fs, ...
+        'duration', 0.050, ...
+        'rampon', 0, ...
+        'rampoff', 0 ...
+        ); 
+    
+    % make clean signal for the whole trial 
+    [x, t] = get_s(...
+                    [1 0 0 0 1 0 0 0 1 0 0 0], ...
+                    0.2, ...
+                    fs, ...
+                    'n_cycles', 32, ...
+                    'ir', ir ...
+                    );
+                
+    [on_off_contrast] = get_on_off(x, fs, 0.8, 0.050);
+    
+    assert(on_off_contrast == 1);
+    
+end
+
+
+function test_get_on_off_perfect_equal(test_case)
+    
+    fs = 500;
+    
+    ir = get_square_kernel(fs, ...
+        'duration', 0.050, ...
+        'rampon', 0, ...
+        'rampoff', 0 ...
+        ); 
+    
+    % make clean signal for the whole trial 
+    [x, t] = get_s(...
+                    [1 0 1 0 1 0 1 0 1 0 1 0], ...
+                    0.2, ...
+                    fs, ...
+                    'n_cycles', 32, ...
+                    'ir', ir ...
+                    );
+                
+    [on_off_contrast] = get_on_off(x, fs, 0.8, 0.050);
+    
+    assert(on_off_contrast == 0);
+    
+end
+
+
+function test_get_on_off_perfect_offbeat(test_case)
+    
+    fs = 500;
+    
+    ir = get_square_kernel(fs, ...
+        'duration', 0.050, ...
+        'rampon', 0, ...
+        'rampoff', 0 ...
+        ); 
+    
+    % make clean signal for the whole trial 
+    [x, t] = get_s(...
+                    [0 0 1 0 0 0 1 0 0 0 1 0], ...
+                    0.2, ...
+                    fs, ...
+                    'n_cycles', 32, ...
+                    'ir', ir ...
+                    );
+                
+    [on_off_contrast] = get_on_off(x, fs, 0.8, 0.050);
+    
+    assert(on_off_contrast == -1);
+    
+end
+
+
+function test_get_on_off_emphasis(test_case)
+    
+    fs = 500;
+    
+    ir = get_square_kernel(fs, ...
+        'duration', 0.100, ...
+        'rampon', 0, ...
+        'rampoff', 0 ...
+        ); 
+    
+    emph_magns = [0, 0.1, 0.5, 1]; 
+    
+    res = nan(1, length(emph_magns)); 
+    
+    for i_emph=1:length(emph_magns)
+        
+        % make clean signal for the whole trial 
+        [x_trial, t] = get_s(...
+                        [1 1 1 1 0 1 1 1 0 0 1 0], ...
+                        0.2, ...
+                        fs, ...
+                        'n_cycles', 32, ...
+                        'ir', ir, ...
+                        'emph_period', 4, ...
+                        'emph_magn', emph_magns(i_emph) ...
+                        );
+
+        x = []; 
+        x(:, 1, 1, 1, 1, :) = repmat(x_trial, [10, 1]); 
+
+        [on_off_contrast] = get_on_off(x, fs, 0.8, 0.050);
+    
+        assert(length(unique(on_off_contrast)) == 1);
+        
+        res(i_emph) = on_off_contrast(1); 
+        
+    end
+    
+    % we should get increasing values of the on/off contrast with increasing
+    % on-beat emphasis
+    assert(all(sort(res) == res))
+    
+end
+
+
+
+
+
+
+
+
+
+
+
