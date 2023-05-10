b0937918a6a40fb5e8b55f60c89cfeae41442454
Tomas Lenc <tomas.lenc@gmail.com>
2023-05-10 10:39:02 +0200

    add phase-locking jitter test

--- tests/test_phase_locking.m
+++ tests/test_phase_locking.m
@@ -40,7 +40,7 @@ function test_get_phase_locking_perfect_periodic(test_case)
 end
 
 
-function test_get_phase_locking_multiD(test_case)
+function test_get_phase_locking_perfect_periodic_multiD(test_case)
     
     fs = 500;
     
@@ -64,7 +64,7 @@ function test_get_phase_locking_multiD(test_case)
     
     [r, theta, ph] = get_phase_locking(x, 1.25, fs, 'skip_s_buffer', 10);
     
-    % check that the resulting vector is equal to 1
+    % check that all the results are equal to 1
     assert(length(unique(r)) == 1);
     assert(length(unique(theta)) == 1);
 
@@ -72,7 +72,40 @@ function test_get_phase_locking_multiD(test_case)
 end
 
 
+function test_get_phase_locking_jitter(test_case)
+    
+    fs = 500;
+    
+    ir = get_square_kernel(fs, ...
+        'duration', 0.100, ...
+        'rampon', 0, ...
+        'rampoff', 0 ...
+        ); 
+    
+    jitters = [0, 0.020, 0.050, 0.070]; 
+    
+    r = nan(1, length(jitters));
+    
+    for i_jit=1:length(jitters)
+        
+        % make signal for the whole trial 
+        [x, t] = get_s(...
+                        [1 0 0 0 1 0 0 0 1 0 0 0], ...
+                        0.2, ...
+                        fs, ...
+                        'n_cycles', 32, ...
+                        'ir', ir, ...
+                        'jitter', jitters(i_jit) ...
+                        );
 
+        r(i_jit) = get_phase_locking(x, 1.25, fs, 'skip_s_buffer', 10);
+        
+    end
+    
+    % check that r is gradually decreasing with increaasing amount of jitter
+    assert(all(sort(r, 'descend') == r))
+    
+end
 
 
 
