8748daf640b2e2f4018657f22f613bc7b492a660
tomo <tomas.lenc@gmail.com>
2024-03-18 18:05:08 +0100

    add function to flexibly convert array of numbers to formatted string

---
+++ src/utils/vec_to_str.m
@@ -0,0 +1,14 @@
+function x_str = vec_to_str(vec, varargin)
+
+parser = inputParser(); 
+
+addParameter(parser, 'format', '%.0f'); 
+addParameter(parser, 'sep', ''); 
+
+parse(parser, varargin{:}); 
+
+format_str = parser.Results.format; 
+sep_str = parser.Results.sep; 
+
+x_str = strjoin(cellfun(@(x) num2str(x, format_str), ...
+            num2cell(vec, 1), 'uni', 0), sep_str); 
