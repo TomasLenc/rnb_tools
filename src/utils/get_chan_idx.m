fee93532c3134f8ea4a305a2c00d6c013e405098
tomo <tomas.lenc@gmail.com>
2024-07-15 12:30:29 +0200

    allow get_chan_idx function to take different argument types

--- src/utils/get_chan_idx.m
+++ src/utils/get_chan_idx.m
@@ -1,4 +1,18 @@
-function idx = get_chan_idx(header, chan_labels)
+function idx = get_chan_idx(header_chanlocs_or_labels, chan_labels)
 
-idx = cellfun(@(x)find(strcmpi({header.chanlocs.labels},x)),...
-                           chan_labels, 'uni', 1);
+% determine if this is header, chanlocs, or labels
+if iscell(header_chanlocs_or_labels)
+    % we got a cell of label strings
+    labs = header_chanlocs_or_labels; 
+elseif isstruct(header_chanlocs_or_labels) && isfield(header_chanlocs_or_labels, 'chanlocs')
+    % we got a header 
+    labs = {header_chanlocs_or_labels.chanlocs.labels}; 
+elseif isstruct(header_chanlocs_or_labels) && isfield(header_chanlocs_or_labels, 'labels')
+    % we got a chanlocs structure
+    labs = {header_chanlocs_or_labels.labels}; 
+else
+    error('cannot figure out what the input is...'); 
+end
+
+idx = cellfun(@(x)find(strcmpi(labs, x)),  chan_labels, 'uni', 1);
+
