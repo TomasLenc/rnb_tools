afd02b1edf8b8b69bd5c1df912de0fc0f05b5917
tomo <tomas.lenc@gmail.com>
2025-05-05 14:17:42 +0200

    update segment_safe: allow zero padding if out of range

--- src/analysis/segment_safe.m
+++ src/analysis/segment_safe.m
@@ -11,8 +11,16 @@ function [header_out, data_out] = segment_safe(header, data, segment_codes, vara
 % data : letswave6 data
 % segment_codes : cell of strings
 %     Event codes that will be used for segmentation. 
-% ignore_out_of_range : bool
-%     If true, epochs out of range will be ignored (but wargning still issued).
+% out_of_range : str, {'error', 'ignore', 'zero_pad'}, optional, default='error' 
+%     If the requested epoch goes out of the range of the continous data,
+%     we need to decide what to do (original lw6 simply returned zeroes for
+%     that epoch without any warning). Here, we can either let the function
+%     raise an error, ignore the out-of-range epoch, or pad the
+%     out-of-range portion of that epoch with zeroes. 
+% keep_other_events : bool, optional, default=true
+%     If true, any events with a code that's not in `segment_codes` will be
+%     kept in the epoched data. This can be useful to, e.g. keep an event
+%     about behavioral response in each trial etc. 
 % 
 % Returns
 % -------
@@ -24,13 +32,15 @@ parser = inputParser;
 
 addParameter(parser, 'x_start', []);
 addParameter(parser, 'x_duration', []);
-addParameter(parser, 'ignore_out_of_range', false);
+addParameter(parser, 'out_of_range', 'error'); % 'error', 'ignore', 'zero_pad'
+addParameter(parser, 'keep_other_events', true); 
 
 parse(parser, varargin{:});
 
 x_start = parser.Results.x_start;
 x_duration = parser.Results.x_duration;
-ignore_out_of_range = parser.Results.ignore_out_of_range;
+out_of_range_treatment = parser.Results.out_of_range;
+keep_other_events = parser.Results.keep_other_events;
 
 
 if isempty(x_start)
@@ -62,20 +72,19 @@ codes_in_header = {events_in_header.code};
 idx_matching_event = find(cellfun(@(x) ismember(x, segment_codes), ...
                           codes_in_header));
 
-n_events = length(idx_matching_event);
+n_ep = length(idx_matching_event);
 
 % number of samples for the segment
 n_x = round(x_duration / header.xstep);
 
 % allocate data output with correct shape
 data_out = nan(...
-    n_events, size(data, 2), size(data, 3), size(data, 4), size(data, 5), n_x...
+    n_ep, size(data, 2), size(data, 3), size(data, 4), size(data, 5), n_x...
     ); 
        
 % allocate output event structure  
-names = fieldnames(events_in_header); 
-names = [names; {'latency_orig'}]; 
-events_out = cell2struct(cell(numel(names), numel(events_in_header)), names); 
+event_fieldnames = fieldnames(events_in_header); 
+event_fieldnames = [event_fieldnames; {'latency_orig'}]; 
 
 % inti counter for output event sturct
 c_event_out = 1;
@@ -83,6 +92,9 @@ c_event_out = 1;
 % also we need another counter that will keep track of which epoch in the
 % output array we are currently at. Note that this can be different from
 % the event number as some events may be skipped due to being out-of-range.
+% Likewise, we may want to keep other events (e.g. response events) from the
+% continuous data - so one epoch can end up having several triggers inside
+% it. 
 c_epoch_out = 1;
 
 % Here we will save the indices of events that ended up being out of range
@@ -95,7 +107,7 @@ fprintf('preparing epoched data...\n');
 
 % go over each event that contains a code matching what the user wants to
 % use for segmentation  
-for i_event=1:n_events
+for i_event=1:n_ep
       
     % take that matching event out of the header
     event = events_in_header(idx_matching_event(i_event)); 
@@ -109,9 +121,11 @@ for i_event=1:n_events
     % get the sample index for the begining of the segment 
     ep_onset_idx = round((ep_latency - header.xstart) / header.xstep); 
     
-    % check if the start index is out of data range and issue warning/error
+    % check if any part of the segment is out of data range and issue warning/error
     if ep_onset_idx+n_x > header.datasize(end) || ep_onset_idx+1 < 1 
-        if ignore_out_of_range
+        
+        if strcmp(out_of_range_treatment, 'ignore')
+            
             warning(...
                 'trying to segment from %.3f to %.3f but data is only from %.3f to %.3f ... ignoring this segment...', ...
                 ep_latency, ...
@@ -121,7 +135,48 @@ for i_event=1:n_events
                 );
             out_of_range_events = [out_of_range_events, i_event];
             continue
+            
+        elseif strcmp(out_of_range_treatment, 'zero_pad')
+            
+            warning(...
+                'trying to segment from %.3f to %.3f but data is only from %.3f to %.3f ... \nI will zero pad the missing data ...', ...
+                ep_latency, ...
+                ep_latency + x_duration, ...
+                header.xstart, ...
+                header.datasize(end) * header.xstep - header.xstart ...
+                );
+            
+            % prepare zero padding 
+            if ep_onset_idx+1 < 1 
+                n_pad = - ep_onset_idx; 
+                data_start_idx = 1; 
+                pad_shape = header.datasize; 
+                pad_shape(6) = n_pad; 
+                pad_before = zeros(pad_shape);                
+            else
+                pad_before = []; 
+                data_start_idx = ep_onset_idx+1; 
+            end
+            
+            if ep_onset_idx+n_x > header.datasize(end)
+                n_pad = (ep_onset_idx+n_x) - header.datasize(end); 
+                data_end_idx = header.datasize(end); 
+                pad_shape = header.datasize; 
+                pad_shape(6) = n_pad; 
+                pad_after = zeros(pad_shape); 
+            else
+                pad_after = []; 
+                data_end_idx = ep_onset_idx+n_x; 
+            end
+            
+            % extract the data setment, zero pad, and assign to the output array
+            data_out(i_event, :, :, :, :, :) = ...
+                cat(6, pad_before, ...
+                       data(event_epoch, :, :, :, :, data_start_idx : data_end_idx), ...
+                       pad_after);
+            
         else
+            
             error(...
                 'segment_safe:SegmentOutOfDataRange', ...
                 'trying to segment from %.3f to %.3f but data is only from %.3f to %.3f', ...
@@ -131,18 +186,21 @@ for i_event=1:n_events
                 header.datasize(end) * header.xstep - header.xstart ...
                 );
         end
-    end
+        
+    else
     
-    % assign the data setment to the output array
-    data_out(i_event, :, :, :, :, :) = ...
-        data(event_epoch, :, :, :, :, ep_onset_idx+1 : ep_onset_idx+n_x);
+        % assign the data setment to the output array
+        data_out(i_event, :, :, :, :, :) = ...
+            data(event_epoch, :, :, :, :, ep_onset_idx+1 : ep_onset_idx+n_x);
+        
+    end
     
     % --------------------------------------------------------------------
     % write event to output structure 
     
-    for i_name=1:length(names)
-        if isfield(event, names{i_name})
-            events_out(c_event_out).(names{i_name}) = event.(names{i_name}); 
+    for i_name=1:length(event_fieldnames)
+        if isfield(event, event_fieldnames{i_name})
+            events_out(c_event_out).(event_fieldnames{i_name}) = event.(event_fieldnames{i_name}); 
         end
     end
     
@@ -161,61 +219,76 @@ for i_event=1:n_events
     % --------------------------------------------------------------------
     % other events 
     
-    % check if there are any other events falling into this new epoch
-    other_events = events_in_header;
-    other_events(idx_matching_event(i_event)) = [];
-    
-    other_events_idx = ...
-        find([[other_events.epoch] == event_epoch & ...
-             [other_events.latency] > ep_latency & ...
-             [other_events.latency] < ep_latency + x_duration ...
-             ]);
-    
-    % It may happen that the next/prev event that the user is asking to use
-    % for segmentation falls into the current epoch (this can be for
-    % example when one is segmenting trials with a buffer time before/after
-    % the trial, and there is prev/next trial event that falls into that
-    % time range). Let's get rid of those events. 
-    other_events_idx_ignore = find(...
-                      cellfun(@(x) ismember(x, segment_codes), ...
-                      {other_events(other_events_idx).code})...
-                      );
-                  
-    % ignore the events that are used for the current epoching (and issue
-    % warning)
-    if ~isempty(other_events_idx_ignore)
+    if keep_other_events
         
-        removed_codes = join(...
-            {other_events(other_events_idx(other_events_idx_ignore)).code},...
-            ', '...
-            );
-        
-        warning('removing additional events "%s" from epoch %d', ...
-                removed_codes{1}, i_event);
+        % check if there are any other events falling into this new epoch
+        other_events = events_in_header;
+        other_events(idx_matching_event(i_event)) = [];
 
-        other_events_idx(other_events_idx_ignore) = [];
-                     
-    end
-       
-    % write additional events that fall into the segment
-    for i_add_event=1:length(other_events_idx)
-                
-        events_out(c_event_out) = other_events(...
-                                        other_events_idx(i_add_event)); 
-                
-        % fix latency so it is wrt the event used for segmentation 
-        events_out(c_event_out).latency = ...
-                events_out(c_event_out).latency - ...
-                events_in_header(idx_matching_event(i_event)).latency;
+        other_events_idx = ...
+            find([[other_events.epoch] == event_epoch & ...
+                 [other_events.latency] > ep_latency & ...
+                 [other_events.latency] < ep_latency + x_duration ...
+                 ]);
 
-        % set the correct epoch in the output array
-        events_out(c_event_out).epoch = i_event;
-        
-        % update the counter for output event structure 
-        c_event_out = c_event_out + 1;
-        
+        % It may happen that the next/prev event that the user is asking to use
+        % for segmentation falls into the current epoch (this can be for
+        % example when one is segmenting trials with a buffer time before/after
+        % the trial, and there is prev/next trial event that falls into that
+        % time range). Let's get rid of those events. 
+        other_events_idx_ignore = find(...
+                          cellfun(@(x) ismember(x, segment_codes), ...
+                          {other_events(other_events_idx).code})...
+                          );
+
+        % ignore the events that are used for the current epoching (and issue
+        % warning)
+        if ~isempty(other_events_idx_ignore)
+
+            removed_codes = join(...
+                {other_events(other_events_idx(other_events_idx_ignore)).code},...
+                ', '...
+                );
+
+            warning('removing additional events "%s" from epoch %d', ...
+                    removed_codes{1}, i_event);
+
+            other_events_idx(other_events_idx_ignore) = [];
+
+        end
+
+        % write additional events that fall into the segment
+        for i_add_event=1:length(other_events_idx)
+
+            event = other_events(other_events_idx(i_add_event)); 
+            
+            for i_name=1:length(event_fieldnames)
+                if isfield(event, event_fieldnames{i_name})
+                    events_out(c_event_out).(event_fieldnames{i_name}) = event.(event_fieldnames{i_name}); 
+                end
+            end
+           
+            % fix latency so it is wrt the event used for segmentation 
+            events_out(c_event_out).latency = ...
+                    events_out(c_event_out).latency - ...
+                    events_in_header(idx_matching_event(i_event)).latency;
+
+                
+            % include original latency 
+            events_out(c_event_out).latency_orig = event.latency; 
+                
+            % set the correct epoch in the output array
+            events_out(c_event_out).epoch = i_event;
+
+            % update the counter for output event structure 
+            c_event_out = c_event_out + 1;
+
+        end
+    
     end
     
+    % --------------------------------------------------------------------
+    % update epoch counter 
     c_epoch_out = c_epoch_out + 1;
         
 end
--- tests/test_segmentation.m
+++ tests/test_segmentation.m
@@ -158,6 +158,11 @@ function test_segment_safe_multi_cond_overlap(test_case)
     assert(all(strcmp(...
         {header_ep.events([header_ep.events.latency] ~= 0).code}, '3' ...
         )));
+        
+%     % visual check 
+%     struct2table(header.events)
+%     struct2table(header_ep.events)
+    
 end
 
 
@@ -207,7 +212,7 @@ function test_segment_safe_too_long_ignore(test_case)
         header, data, {'1'}, ...
         'x_start', 0, ...
         'x_duration', trial_dur + 0.5, ...
-        'ignore_out_of_range', true);
+        'out_of_range', 'ignore');
     
     assert(header_ep.datasize(1) == 4);
     assert(isequal([header_ep.events.epoch], [1 : n_trials-1]));
@@ -215,6 +220,110 @@ function test_segment_safe_too_long_ignore(test_case)
     assert(all(cellfun(@(x) str2num(x), {header_ep.events.code}) == ...
                data_ep(:, 1, 1, 1, 1, 1)'));
         
+end
+
+
+function test_segment_safe_too_long_zero_pad(test_case)
+    % Test the case when the requested epoch goes outside of data range (it
+    % ends after the data ends). This can happen e.g. when we ask for too
+    % much buffer after trial end but the acquisition has been stopped. We
+    % will zero pad the missing data here. 
+    n_cond = 1;
+    n_trials = 5;
+    trial_dur = 10;
+    pause_dur = 3;
+
+    [header, data] = make_lw_dataset(...
+        'n_cond', n_cond, ...
+        'n_trials', n_trials,...
+        'trial_dur', trial_dur, ...
+        'pause_dur', pause_dur,...
+        'end_recording_buffer', 0.010, ...
+        'fs', 1000);
+
+    [header_ep, data_ep] = segment_safe(...
+        header, data, {'1'}, ...
+        'x_start', 0, ...
+        'x_duration', trial_dur + 0.5, ...
+        'out_of_range', 'zero_pad');
+    
+    assert(header_ep.datasize(1) == n_trials);
+    assert(isequal([header_ep.events.epoch], [1 : n_trials]));
+    assert(all([header_ep.events.latency] == 0));
+
+    % make sure the data value at time 0 is exactly equal to the event code
+    % in that epoch 
+    assert(all(cellfun(@(x) str2num(x), {header_ep.events.code}) == ...
+               data_ep(:, 1, 1, 1, 1, 1)'));
+     
+    % find indices where the padding should be       
+    expected_N_pad = (header.events(end).latency / header.xstep + (trial_dur + 0.5) / header.xstep) - ...
+                      header.datasize(end); 
+                  
+   % ensure the padding is in the last epoch 
+   assert(all(data_ep(end, 1, 1, 1, 1, end-expected_N_pad+1 : end) == 0))
+   
+   % in the last epoch, the value just before padding shouldn't be zero 
+   assert(data_ep(end, 1, 1, 1, 1, end-expected_N_pad) ~= 0)
+
+   % all other epochs shouldn't have any padding
+   for i=1:n_trials-1
+        assert(~all(data_ep(i, 1, 1, 1, 1, end-expected_N_pad+1 : end) == 0))
+   end
+   
+ 
+end
+
+
+function test_segment_safe_too_late_zero_pad(test_case)
+    % Test the case when the requested epoch starts before the continuous
+    % data started. This can happen e.g. when we ask for too much buffer
+    % before trial onset. We will zero pad the missing data here.
+    n_cond = 1;
+    n_trials = 5;
+    trial_dur = 10;
+    pause_dur = 3;
+
+    [header, data] = make_lw_dataset(...
+        'n_cond', n_cond, ...
+        'n_trials', n_trials,...
+        'trial_dur', trial_dur, ...
+        'pause_dur', pause_dur,...
+        'start_recording_buffer', 0.010, ...
+        'end_recording_buffer', 0.010, ...
+        'fs', 1000);
+
+    [header_ep, data_ep] = segment_safe(...
+        header, data, {'1'}, ...
+        'x_start', -0.5, ...
+        'x_duration', trial_dur + 0.5, ...
+        'out_of_range', 'zero_pad');
+    
+    assert(header_ep.datasize(1) == n_trials);
+    assert(isequal([header_ep.events.epoch], [1 : n_trials]));
+    assert(all([header_ep.events.latency] == 0));
+    
+    % make sure the data value at time 0 is exactly equal to the event code
+    % in that epoch 
+    idx_time_zero = 0.5 / header.xstep + 1; 
+    assert(all(cellfun(@(x) str2num(x), {header_ep.events.code}) == ...
+               data_ep(:, 1, 1, 1, 1, idx_time_zero)'));
+     
+    % find indices where the padding should be       
+    expected_N_pad = (0.5 - 0.010) / header.xstep; 
+    
+    % ensure the padding is in the first epoch 
+    assert(all(data_ep(1, 1, 1, 1, 1, 1:expected_N_pad) == 0))
+                 
+    % in the first epoch, the value just after padding shouldn't be zero 
+    assert(data_ep(1, 1, 1, 1, 1, expected_N_pad+1) ~= 0)
+    
+    % all other epochs shouldn't have any padding
+    for i=2:n_trials
+        assert(~all(data_ep(i, 1, 1, 1, 1, 1:expected_N_pad) == 0))
+    end
+
+ 
 end
 
 
@@ -346,7 +455,7 @@ function test_segment_safe_resegment_too_long_ignore(test_case)
         header, data, {'1', '3', '4'},...
         'x_start', -ep_buffer, ...
         'x_duration', trial_dur + pause_dur + 2, ...
-        'ignore_out_of_range', true);
+        'out_of_range', 'ignore');
     
     % we should have 3 events in the output
     assert(length(header_ep.events) == 3);
@@ -355,28 +464,25 @@ function test_segment_safe_resegment_too_long_ignore(test_case)
     assert(size(data_ep, 1) == 2);
     
     % first, there should be event code "1". Everything fine. 
-    assert(isequal(...
-        header_ep.events(1),...
-        struct('code', '1', 'latency', 0, 'epoch', 1)...
-        )); 
-    
+    assert(strcmp(header_ep.events(1).code, '1'))
+    assert(header_ep.events(1).latency == 0)
+    assert(header_ep.events(1).epoch == 1)
+
     % But for the first epoch, there was an overlaping event with code "2".
     % Now we didn't ask to segment based on code "2" so the event will be
     % registered in the output
-    assert(isequal(...
-        header_ep.events(2), ...
-        struct('code', '2', 'latency', trial_dur + pause_dur, 'epoch', 1)...
-        ));     
-    
+    assert(strcmp(header_ep.events(2).code, '2'))
+    assert(header_ep.events(2).latency == trial_dur + pause_dur)
+    assert(header_ep.events(2).epoch == 1)
+        
     % The second epoch is based on the following trigger "3" and even
     % though there is an overlap with the following trigger "4" within the
     % duration of the epoch, that one is ignored since it's one of the
     % codes used for the current segmentation (the function "knows" this is
     % just the following epoch)
-    assert(isequal(...
-        header_ep.events(3), ...
-        struct('code', '3', 'latency', 0, 'epoch', 2)...
-        )); 
+    assert(strcmp(header_ep.events(3).code, '3'))
+    assert(header_ep.events(3).latency == 0)
+    assert(header_ep.events(3).epoch == 2)
     
     % another check - the simualtino function placed an impulse in the data
     % at the start of each trial - we can make sure we got the correct data
@@ -384,22 +490,23 @@ function test_segment_safe_resegment_too_long_ignore(test_case)
     idx0 = round(ep_buffer / header_ep.xstep + 1); 
     assert(isequal(data_ep(:, 1, 1, 1, 1, idx0), [1; 3]));
 
-    % re-epoch
+    % re-epoch, this time include all event codes, but ignore out-of-range
+    % epochs -> this should remove the event '2' that was in the first
+    % epoch 
     [header_ep, data_ep] = segment_safe(...
         header_ep, data_ep, {'1', '2', '3', '4'},...
         'x_start', 0, ...
         'x_duration', trial_dur-6, ...
-        'ignore_out_of_range', true);
+        'out_of_range', 'ignore');
     
-    assert(isequal(...
-        header_ep.events(1),...
-        struct('code', '1', 'latency', 0, 'epoch', 1)...
-        )); 
-    assert(isequal(...
-        header_ep.events(2), ...
-        struct('code', '3', 'latency', 0, 'epoch', 2)...
-        )); 
+    assert(strcmp(header_ep.events(1).code, '1'))
+    assert(header_ep.events(1).latency == 0)
+    assert(header_ep.events(1).epoch == 1)
     
+    assert(strcmp(header_ep.events(2).code, '3'))
+    assert(header_ep.events(2).latency == 0)
+    assert(header_ep.events(2).epoch == 2)
+       
     assert(length(header_ep.events) == 2);
     assert(size(data_ep, 1) == 2);
     
@@ -437,18 +544,24 @@ function test_segment_safe_additional_event_fields(test_case)
         header, data, {'1', '3', '4'},...
         'x_start', -ep_buffer, ...
         'x_duration', trial_dur + pause_dur + 2, ...
-        'ignore_out_of_range', true);
+        'out_of_range', 'ignore');
     
     assert(isequal(...
         header_ep.events(1),...
-        struct('code', '1', 'latency', 0, 'epoch', 1, 'polarity', 'a')...
+        struct('code', '1', 'latency', 0, 'epoch', 1, 'polarity', 'a', ...
+               'latency_orig', start_recording_buffer)...
         )); 
+    
     assert(isequal(...
         header_ep.events(2), ...
-        struct('code', '2', 'latency', trial_dur + pause_dur, 'epoch', 1, 'polarity', 'b')...
+        struct('code', '2', 'latency', trial_dur + pause_dur, 'epoch', 1, 'polarity', 'b', ...
+               'latency_orig', start_recording_buffer + trial_dur + pause_dur)...
         ));     
+    
     assert(isequal(...
         header_ep.events(3), ...
-        struct('code', '3', 'latency', 0, 'epoch', 2, 'polarity', 'c')...
+        struct('code', '3', 'latency', 0, 'epoch', 2, 'polarity', 'c', ...
+               'latency_orig', start_recording_buffer + 2*(trial_dur + pause_dur))...
         )); 
+    
 end
